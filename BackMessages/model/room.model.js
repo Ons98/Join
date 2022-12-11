0b80e0c9c8599b86b9d4c25061c0a0ef77e60105
Ons Lakhal <ons.lakhel@gmail.com>
2022-12-11 22:02:59 +0100

    ''

--- BackMessages/model/room.model.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-var roomSchema = new mongoose.Schema({
-    name: String
-})
-mongoose.model('room', roomSchema)
--- BackMessages/model/user.model.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-var userSchema = new mongoose.Schema({
-    pseudo: String
-})
-mongoose.model('user', userSchema)
