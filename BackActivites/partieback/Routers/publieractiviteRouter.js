17c2f894d8eeb58373716649de6e7973f90b4aee
Ons Lakhal <ons.lakhel@gmail.com>
2022-12-11 22:10:53 +0100

    ''

--- BackActivites/partieback/Routers/publieractiviteRouter.js
+++
@@ -1,8 +0,0 @@
-const router=require('express').Router()
-const publieractiviteController=require('../Controllers/publieractiviteController')
-router.post("/createPublieractivite",publieractiviteController.createPublieractivite)
-router.get("/getAllPubActivites",publieractiviteController.getAllPubActivites)
-router.get("/getActid/:id",publieractiviteController.getActid)
-
-module.exports = router
-
